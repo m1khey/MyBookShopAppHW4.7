eb5104c3f99ebf6bf6d8281b3e94a7f7e696f397
Михаил Ольхови <ermo1ov@yandex.ru>
2022-04-19 23:01:39 +0300

    Lesson 6.4-6.5

--- src/main/java/com/example/MyBookShopApp/repositoryes/BookRepository.java
+++ src/main/java/com/example/MyBookShopApp/repositoryes/BookRepository.java
@@ -1,5 +1,6 @@
 package com.example.MyBookShopApp.repositoryes;
 
+import org.hibernate.cache.spi.entry.StructuredCacheEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import skbx.example.struct.Book;
@@ -11,4 +12,32 @@ public interface BookRepository extends JpaRepository<Book,Integer> {
 
     @Query("from Book")
     List<Book> customFindAllBooks();
+
+    //NEW BOOK REST REPOSITORY COMMANDS
+
+    List<Book> findBooksByAuthorFirstNameContaining(String authorFirstName);
+
+    List<Book> findBooksByTitleContaining(String bookTitle);
+
+    List<Book> findBooksByPriceOldBetween(Integer min, Integer max);
+
+    List<Book> findBooksByPriceOldIs(Integer price);
+
+    @Query("from Book where isBestseller=1")
+    List<Book> getBestseller();
+
+    @Query(value="SELECT * FROM books WHERE discount = (SELECT MAX(discount) FROM books)", nativeQuery = true)
+    List<Book> getBooksWithMaxDiscount();
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/example/MyBookShopApp/services/BookService.java
+++ src/main/java/com/example/MyBookShopApp/services/BookService.java
@@ -20,4 +20,32 @@ public class BookService {
     public List<Book> getBooksData() {
         return bookRepository.findAll();
     }
+
+    //NEW BOOK SERVICE METHODS
+
+    public List<Book> getBooksByAuthor(String authorName){
+        return bookRepository.findBooksByAuthorFirstNameContaining(authorName);
+    }
+
+    public List<Book> getBooksByTitle(String title){
+        return bookRepository.findBooksByTitleContaining(title);
+    }
+
+    public List<Book> getBooksWithPriceBetween(Integer min, Integer max){
+        return bookRepository.findBooksByPriceOldBetween(min,max);
+    }
+
+    public List<Book> getBooksWithPrice(Integer price){
+        return bookRepository.findBooksByPriceOldIs(price);
+    }
+
+    public List<Book> getBooksWithMaxPrice(){
+        return bookRepository.getBooksWithMaxDiscount();
+    }
+
+    public List<Book> getBestsellers(){
+        return bookRepository.getBestseller();
+    }
+
+
 }
